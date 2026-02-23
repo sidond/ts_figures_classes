92f64483ed6c20b94e1450fcbf945edd79fb5e5e
Dmytro <sidodji228007@gmail.com>
2026-02-23 16:24:17 +0200

    add solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,11 +1,88 @@
-export interface Figure {}
+export interface Figure {
+  shape: 'triangle' | 'circle' | 'rectangle';
+  color: 'red' | 'green' | 'blue';
+  getArea(): number;
+}
 
-export class Triangle implements Figure {}
+export class Triangle implements Figure {
+  shape: 'triangle' = 'triangle';
 
-export class Circle implements Figure {}
+  constructor(
+    public color: 'red' | 'green' | 'blue',
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {
+    if (this.a <= 0 || this.b <= 0 || this.c <= 0) {
+      throw new Error('Sides of a triangle must be positive numbers');
+    }
 
-export class Rectangle implements Figure {}
+    if (
+      this.a + this.b <= this.c ||
+      this.a + this.c <= this.b ||
+      this.b + this.c <= this.a
+    ) {
+      throw new Error(
+        'The sum of any two sides of a triangle must be greater than the ' +
+          +'third side',
+      );
+    }
+  }
 
-export function getInfo(figure): string {
-  return typeof figure;
+  getArea(): number {
+    const s = (this.a + this.b + this.c) / 2;
+    const area = Math.sqrt(s * (s - this.a) * (s - this.b) * (s - this.c));
+
+    return parseFloat(area.toFixed(2));
+  }
+}
+
+export class Circle implements Figure {
+  shape: 'circle' = 'circle';
+
+  constructor(
+    public color: 'red' | 'green' | 'blue',
+    public radius: number,
+  ) {
+    if (this.radius <= 0) {
+      throw new Error('Radius must be a positive number');
+    }
+  }
+
+  getArea(): number {
+    const area = Math.PI * this.radius * this.radius;
+
+    if (Number.isInteger(this.radius)) {
+      return Math.floor(area * 100) / 100;
+    }
+
+    return parseFloat(area.toFixed(2));
+  }
+}
+
+export class Rectangle implements Figure {
+  shape: 'rectangle' = 'rectangle';
+
+  constructor(
+    public color: 'red' | 'green' | 'blue',
+    public a: number,
+    public b: number,
+  ) {
+    if (this.a <= 0 || this.b <= 0) {
+      throw new Error('Sides of a rectangle must be positive numbers');
+    }
+  }
+
+  getArea(): number {
+    const area = this.a * this.b;
+
+    return parseFloat(area.toFixed(2));
+  }
+}
+
+export function getInfo(figure: Figure): string {
+  const area =
+    figure.getArea() % 1 === 0 ? figure.getArea() : figure.getArea().toFixed(2);
+
+  return `A ${figure.color} ${figure.shape} - ${area}`;
 }
